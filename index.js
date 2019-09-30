25c8db8fecbb82b0f88934c888fec421f75b1127
calbodragon <cristhiambohorquez@yahoo.es>
2019-09-30 12:35:22 -0500

    act

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public'))); // archivos que no interactuan con el backend, no envian ni reciben información.
 
-app.listen(app.get('port'), () => {
+const server = app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
+});
+
+// Debemos crear otro puerto diferente al que ya está funcionando para el servidor de socket
+const socketIO = require('socket.io');
+const io = socketIO(server);
+
+// cada que actualizamos la pagina nos da un nuevo socket, en consola se puede ver
+io.on('connection', (socket) => {
+    console.log('user connected with socket: ', socket.id);
+    socket.on('messages', (data => {       
+        console.log(data);
+        socket.emit('messages', data);
+    })
+    );
+    socket.on('typing', (data) =>{
+        console.log(data + ' esta escribiendo...')
+        socket.broadcast.emit('typing', data); 
+    });
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-//const socket = io();
+const socket = io();
 
 // se traen todas los id tal cual como estan escritas en el html
 let message_user = document.getElementById('message_user');
@@ -7,6 +7,30 @@ let btn = document.getElementById('send');
 let events = document.getElementById('events');
 let messages = document.getElementById('messages');
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function () {
     console.log(user.value + ': ' + message_user.value);
+    socket.emit('messages', {
+        user: user.value,
+        message_user: message_user.value
+    });
 });
+message_user.addEventListener('keypress', function() {
+    socket.emit('typing', user.value);
+}); 
+
+
+
+socket.on('messages', function(data){
+    events.innerHTML = '';
+    messages.innerHTML += `<p>
+    <strong> ${data.user}: </strong>
+    ${data.message_user}
+    </p>`;
+    message_user.value = ''; 
+    events.innerHTML = ''; 
+});
+socket.on('typing', function(data){
+    events.innerHTML = `<p><em>${data} está escribiendo...</em></p>`
+});
+
+
